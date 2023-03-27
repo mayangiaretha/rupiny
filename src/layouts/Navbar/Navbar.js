5e70c447fff6351fa0630506aea46223e91f8d9e
aretha <myaretha2620@gmail.com>
2023-03-27 15:57:52 +0300

    update the various component UI

--- src/layouts/Navbar/Navbar.js
+++ src/layouts/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AppBar from "@mui/material/AppBar";
+import {AppBar} from "@mui/material";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
